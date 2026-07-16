75a8ec787b3add855d9b140af20d1368514d8d22
Evan Moritz <emoritzx@gmail.com>
2026-07-16 02:08:38 -0400

    Address comments

--- data-manager/app/services/event_enrichment_handler.rb
+++ data-manager/app/services/event_enrichment_handler.rb
@@ -1,13 +1,18 @@
+# Makes follow-on requests for additional data related to events
 class EventEnrichmentHandler
 
+    # Constructor
     def initialize(request_publisher)
         @publisher = request_publisher
     end
 
+    # Request additional data for the event
     def handle(event)
         enrich(event, "actor", "url")
     end
 
+    # Extract the URL from a specific field of the event
+    # and publish a follow-on request to the GitHub API
     def enrich(event, top_field_name, url_field_name)
         Rails.logger.debug "Enriching event #{event["id"]}"
         top_field = event[top_field_name]
